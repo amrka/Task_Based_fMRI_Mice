97a8021c0585535b6d5fed96abc3ec63ecb1f1dc
amrka <amr.kamel@uv.es>
2018-05-29 19:05:57 +0200

    Update change_names_copes_1st_level.sh

--- change_names_copes_1st_level.sh
+++ change_names_copes_1st_level.sh
@@ -3,7 +3,7 @@
 #to the first level data of the stimulation experiment instead of doing it
 #from the shell one by one
 
-
+#4 sec
 python change_runs_to_contain_gp_name.py '/media/amr/HDD/Work/Stimulation/10Hz_higher_level_stats/copes' 
 python change_runs_to_contain_gp_name.py '/media/amr/HDD/Work/Stimulation/10Hz_higher_level_stats/varcopes' 
 python change_runs_to_contain_gp_name.py '/media/amr/HDD/Work/Stimulation/10Hz_higher_level_stats/tdof' 
@@ -17,3 +17,19 @@ python change_runs_to_contain_gp_name.py '/media/amr/HDD/Work/Stimulation/20Hz_h
 python change_runs_to_contain_gp_name.py '/media/amr/HDD/Work/Stimulation/40Hz_higher_level_stats/copes' 
 python change_runs_to_contain_gp_name.py '/media/amr/HDD/Work/Stimulation/40Hz_higher_level_stats/varcopes' 
 python change_runs_to_contain_gp_name.py '/media/amr/HDD/Work/Stimulation/40Hz_higher_level_stats/tdof' 
+
+
+#10 sec
+python change_runs_to_contain_gp_name.py '/media/amr/HDD/Work/Stimulation/10Hz_10s_higher_level_stats/copes' 
+python change_runs_to_contain_gp_name.py '/media/amr/HDD/Work/Stimulation/10Hz_10s_higher_level_stats/varcopes' 
+python change_runs_to_contain_gp_name.py '/media/amr/HDD/Work/Stimulation/10Hz_10s_higher_level_stats/tdof' 
+
+
+python change_runs_to_contain_gp_name.py '/media/amr/HDD/Work/Stimulation/20Hz_10s_higher_level_stats/copes' 
+python change_runs_to_contain_gp_name.py '/media/amr/HDD/Work/Stimulation/20Hz_10s_higher_level_stats/varcopes' 
+python change_runs_to_contain_gp_name.py '/media/amr/HDD/Work/Stimulation/20Hz_10s_higher_level_stats/tdof' 
+
+
+python change_runs_to_contain_gp_name.py '/media/amr/HDD/Work/Stimulation/40Hz_10s_higher_level_stats/copes' 
+python change_runs_to_contain_gp_name.py '/media/amr/HDD/Work/Stimulation/40Hz_10s_higher_level_stats/varcopes' 
+python change_runs_to_contain_gp_name.py '/media/amr/HDD/Work/Stimulation/40Hz_10s_higher_level_stats/tdof' 
